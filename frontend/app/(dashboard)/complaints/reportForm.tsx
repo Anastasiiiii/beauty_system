336a64e72b9294d62ed9b306f5e26ebcf05d04b9
anastasiiiii <nastia262002@gmail.com>
2024-12-27 02:20:58 +0100

    change input

--- frontend/app/(dashboard)/complaints/reportForm.tsx
+++ frontend/app/(dashboard)/complaints/reportForm.tsx
@@ -29,20 +29,26 @@ export function ReportForm({ token }: { token: string | undefined }) {
       <label htmlFor="feedback" style={{ display: 'block', marginBottom: '8px' }}>
         Введіть, будь-ласка, вашу пропозицію або скаргу.
       </label>
-      <input
-        id="feedback"
-        name="feedback"
-        type='textArea'
-        placeholder="Введіть свої пропозиції або скарги тут..."
-        style={{
-          width: '100%',
-          padding: '10px',
-          fontSize: '16px',
-          border: '1px solid #ccc',
-          borderRadius: '5px',
-          resize: 'vertical',
-        }}
-      />
+      
+      <div style={{ position: 'relative', width: '100%' }}>
+        {/* Textarea with absolute positioned placeholder */}
+        <textarea
+          id="feedback"
+          name="feedback"
+          placeholder="Введіть свої пропозиції або скарги тут..."
+          style={{
+            width: '100%',
+            height: '150px',
+            padding: '10px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            resize: 'vertical',
+            position: 'relative',
+            zIndex: 1,  
+            background: 'transparent',
+          }}
+        />
+      </div>
 
       <Button style={{ marginTop: '16px' }}>
         Відправити
